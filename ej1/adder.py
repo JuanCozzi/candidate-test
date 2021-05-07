7691486a86a7ece92f887d2e206d4eeca4e20eff
JuanCozzi <cozzi.juan.ignacio@gmail.com>
2021-05-07 17:49:07 -0300

    Ej1: retiro de codigo original comentado

--- ej1/adder.py
+++ ej1/adder.py
@@ -40,28 +40,6 @@ class Stream(Record):
             self.ready <= 0
             return data
 
-""" Used from the example.py 
-class Incrementador(Elaboratable):
-    def __init__(self, width):
-        self.a = Stream(width, name='a')
-        self.r = Stream(width, name='r')
-
-    def elaborate(self, platform):
-        m = Module()
-        sync = m.d.sync
-        comb = m.d.comb
-
-        with m.If(self.r.accepted()):
-            sync += self.r.valid.eq(0)
-
-        with m.If(self.a.accepted()):
-            sync += [
-                self.r.valid.eq(1),
-                self.r.data.eq(self.a.data + 1)
-            ]
-        comb += self.a.ready.eq((~self.r.valid) | (self.r.accepted()))
-        return m
-"""
 
 class Adder(Elaboratable):
     def __init__(self, width):
@@ -93,40 +71,6 @@ async def init_test(dut):
     await RisingEdge(dut.clk)
     await RisingEdge(dut.clk)
     dut.rst <= 0
-
-""" Used from the example.py 
-@cocotb.test()
-async def burst(dut):
-    await init_test(dut)
-
-    stream_input = Stream.Driver(dut.clk, dut, 'a__')
-    stream_output = Stream.Driver(dut.clk, dut, 'r__')
-
-    N = 100
-    width = len(dut.a__data)
-    mask = int('1' * width, 2)
-
-    data = [getrandbits(width) for _ in range(N)]
-    expected = [(d + 1) & mask for d in data]
-    cocotb.fork(stream_input.send(data))
-    recved = await stream_output.recv(N)
-    assert recved == expected
-
-
-if __name__ == '__main__':
-    core = Incrementador(7)
-    run(
-        core, 'example',
-        ports=
-        [
-            *list(core.a.fields.values()),
-            *list(core.r.fields.values())
-        ],
-        vcd_file='incrementador.vcd'
-    )
-"""
-
-
 
 @cocotb.test()
 async def burst(dut):
